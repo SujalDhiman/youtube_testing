62185a14a06d0509f9426268094a461b509b740f
Sujal_Dhiman <sujaldhiman2003@gmail.com>
2024-02-03 16:48:11 +0530

    testing

--- youtube_frontend/src/App.jsx
+++ youtube_frontend/src/App.jsx
@@ -2,10 +2,13 @@ import { useState } from "react";
 import { CreateVideo } from "./componentsCollection";
 import {useForm} from "react-hook-form"
 import axios from "axios";
+import {useSelector} from "react-redux"
 function App() {
+  const authStatus=useSelector(state=>state.auth.authStatus)
+
   return (
     <>
-    <CreateVideo/>
+      {authStatus === false ? <h1>hello</h1>:<h1>bi</h1>}
     </>
   )
 }
