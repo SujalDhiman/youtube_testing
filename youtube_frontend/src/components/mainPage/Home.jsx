f9e5759deb033d96bfea8b99c05eed2652dac7aa
Sujal_Dhiman <sujaldhiman2003@gmail.com>
2024-02-03 17:08:06 +0530

    setting login

--- youtube_frontend/src/components/mainPage/Home.jsx
+++ youtube_frontend/src/components/mainPage/Home.jsx
@@ -1,13 +1,27 @@
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
   const authStatus = useSelector((state) => state.auth.userStatus);
+
+  let navigate=useNavigate()
+
+  function signUp()
+  {
+      navigate("/signUp")
+  }
+
+  function login()
+  {
+    navigate("/login")
+  }
+
   return (
     <nav>
       {!authStatus ? (
         <>
-          <button>SignUp</button>
-          <button>Login</button>
+          <button onClick={signUp}>SignUp</button>
+          <button onClick={login}>Login</button>
         </>
       ) : (
         <button>LogOut</button>
--- youtube_frontend/src/main.jsx
+++ youtube_frontend/src/main.jsx
@@ -4,10 +4,29 @@ import App from "./App.jsx";
 import "./index.css";
 import store from "./reduxtoolkit/store.js";
 import { Provider } from "react-redux";
+import {RouterProvider, createBrowserRouter} from "react-router-dom"
+import { Login } from "./componentsCollection.js";
+
+
+const router=createBrowserRouter([
+  {
+      path:"/",
+      element:<App/>,
+      children:[
+        {
+          path:"/login",
+          element:<Login/>
+        }
+      ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
     </Provider>
   </React.StrictMode>
 );
